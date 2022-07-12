8d09986db9a34b37be28f74a43013aa60b8fcb25
Miyuru <miyurushalitha@gmail.com>
2022-07-12 14:50:48 +0530

    Random walk.

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Ant : MonoBehaviour
 {
-    private bool isStop = true;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float turningAngle = 90.0f;
+
+    int count = 0;
+
+    private void Start()
+    {
+        InvokeRepeating("Turn", 0.0f, 1.0f);
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            isStop = !isStop;
-        }
+        RandomWalk();
+    }
 
-        if (!isStop)
-        {
-            transform.position = transform.position + new Vector3(0.0f, 0.5f, 0.0f) * Time.deltaTime;
-        }
+    private void Turn()
+    {
+        float randomAngle = Random.Range(-turningAngle, turningAngle);
+
+        transform.Rotate(0.0f, 0.0f, randomAngle);
+
+        Debug.Log($"TURN {++count}, Angle {randomAngle}");
+    }
+
+    private void RandomWalk()
+    {
+        transform.position = transform.position + speed * Time.deltaTime * transform.up;
     }
 }
