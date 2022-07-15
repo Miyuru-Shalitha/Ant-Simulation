0c15ef8d57ee4b46dd89baac7babccd41e1f5007
Miyuru <miyurushalitha@gmail.com>
2022-07-15 17:29:42 +0530

    Update fov

--- Assets/Editor/FieldOfViewEditor.cs
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -10,9 +10,12 @@ public class FieldOfViewEditor : Editor
     {
         FieldOfView fow = (FieldOfView)target;
         Handles.color = Color.white;
-        Handles.DrawWireArc(fow.transform.position, Vector3.back, Vector3.up, 360, fow.viewRadius);
+        //Handles.DrawWireArc(fow.transform.position, Vector3.back, Vector3.up, fow.viewAngle, fow.viewRadius);
         Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
         Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        Vector3 fromDir = fow.transform.position + viewAngleA * fow.viewRadius - fow.transform.position;
+        Handles.DrawWireArc(fow.transform.position, Vector3.back, fromDir, fow.viewAngle, fow.viewRadius);
 
         Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
         Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
